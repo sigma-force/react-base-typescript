5165bd5534f1d7e923b6a2e2a3549ba3fffed74a
Andrés Santana <drew.lizcano@gmail.com>
2020-03-09 17:19:02 -0500

    file-loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import '../styles/components/App.styl';
 
-const App = () => <h1>Hello React!</h1>;
+const cat =
+  'https://firebasestorage.googleapis.com/v0/b/platzi-master-d7573.appspot.com/o/gato.gif?alt=media&token=d79bd5e0-4235-4b62-91ea-acf1cf0be1f7';
+const App = () => {
+  return (
+    <div className="app">
+      <img className="cat" src={cat} alt="cat" />
+      <h1>Hello React</h1>
+    </div>
+  );
+};
 
 export default App;
