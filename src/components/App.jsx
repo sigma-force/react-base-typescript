a0f8d4c20b139fb6f63f883234688f143e07cf15
Andrés Santana <drew.lizcano@gmail.com>
2020-03-28 09:39:07 -0500

    design system

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/components/App.styl';
+import '../styles/fundations/typography.styl';
 
 const cat =
   'https://firebasestorage.googleapis.com/v0/b/platzi-master-d7573.appspot.com/o/gato.gif?alt=media&token=d79bd5e0-4235-4b62-91ea-acf1cf0be1f7';
@@ -7,7 +8,9 @@ const App = () => {
   return (
     <div className="app">
       <img className="cat" src={cat} alt="cat" />
-      <h1>Hello React</h1>
+      <h1 className="h1">Hello React</h1>
+      <h1 className="h1">Hello React</h1>
+      <h1 className="h1">Hello React</h1>
     </div>
   );
 };
