a4bc9c9278bfeebc1c6866a0e68b87de41ef4455
Andrés Santana <drew.lizcano@gmail.com>
2020-04-20 14:02:19 -0500

    base

---
+++ src/components/routes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/globalStyles.styl';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="h1">Hello React</h1>
+    </div>
+  );
+};
+
+export default App;
